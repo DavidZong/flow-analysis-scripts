aca5f7ed049d03715460f8de3572c248105e9622
dmz <davidzong1@gmail.com>
2016-08-24 18:14:06 -0500

    added processRun.m

---
+++ processRun.m
@@ -0,0 +1,7 @@
+clc
+clear all
+close all
+
+sort_results('90 Percent Gate','160824_IPTG_repeat.xlsx','160824_90percentoutput')
+sort_results('50 Percent Gate','160824_IPTG_repeat.xlsx','160824_50percentoutput')
+sort_results('10 Percent Gate','160824_IPTG_repeat.xlsx','160824_10percentoutput')
